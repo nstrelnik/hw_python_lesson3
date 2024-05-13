247a39245c3773f118ff7b2abd6b13321a23f3df
Anastasia Strelnik <n.strelnik@mail.ru>
2024-05-13 17:08:36 +0200

    Добавлен фикстура на установку размера окна

---
+++ conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from selene import browser
+
+@pytest.fixture(autouse=True)
+def browser_window_size():
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
+    print("Размер окна установлен")
+    yield
+    browser.quit()
