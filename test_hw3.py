ddaff6d89a67851defdb882badd75f82cd0854f6
Anastasia Strelnik <n.strelnik@mail.ru>
2024-05-13 18:42:02 +0200

    Исправлены негативные тесты

--- test_hw3.py
+++ test_hw3.py
@@ -1,11 +1,20 @@
 from selene import browser, be, have
 
+
 def test_search_from_google():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_search_from_google_not_find():
+
+def test_search_from_google_not_have_text():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('qweqweqwe').press_enter()
-    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    browser.element('[id="search"]').should(have.no.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_google_searching_empty_result():
+    browser.open("https://google.ru")
+    browser.element('[name="q"]').should(be.blank).type('uryytyweuiyruuyt!!!fs,jhjhs').press_enter()
+    browser.element('#botstuff').should(have.text('По запросу uryytyweuiyruuyt!!!fs,jhjhs ничего не найдено.'))
+
